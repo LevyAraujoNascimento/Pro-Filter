ee59ac5a1e2f91d2b271066a4265c8ac8c3db31e
Levy Araujo <levy4raujo@gmail.com>
2023-08-22 16:12:00 -0300

    [Requisito 1 concluído.]
    Corrigindo a função show deepst file.

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,15 @@
 """Arquivo que estudantes devem editar"""
 
 
+def numPath(string):
+    return string.count('/')
+
+
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=numPath)
         print(f"Deepest file: {deepest_file}")
 
 
