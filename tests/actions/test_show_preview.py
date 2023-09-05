30611e4248afdeb7f135b27f67d561231e46b3f7
Levy Araujo <levy4raujo@gmail.com>
2023-09-05 09:00:13 -0300

    [Requisito 3 em andamento]
    Criando as funções para o teste.

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,38 @@
+import pytest
+
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+
+@pytest.fixture
+def context():
+    return {
+      "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+      "all_dirs": ["src", "src/utils"]
+    }
+
+
+@pytest.fixture
+def empty_context():
+    return {
+      "all_files": [],
+      "all_dirs": []
+    }
+
+
+def test_context(context, capsys):
+    show_preview(context)
+    assert (
+        capsys.readouterr().out
+        == """Found 3 files and 2 directories
+First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']
+First 5 directories: ['src', 'src/utils']
+"""
+    )
+
+
+def test_empty_context(empty_context, capsys):
+    show_preview(empty_context)
+    assert (
+        capsys.readouterr().out
+        == """Found 0 files and 0 directories\n"""
+    )
